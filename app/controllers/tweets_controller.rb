bc507f7d26b949b24cc0eed73d3a1c4c6199696e
Scott Hoisington <hoyzington@gmail.com>
2020-02-09 23:18:05 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,23 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
+  
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+  
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+  
+  post '/tweets/:id/edit' do
+    erb :'/tweets/show'
+  end
+  
+  get '/tweets/:id' do
+    erb :'/tweets/show_tweet'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
 
+  get '/' do
+    erb :index
+  end
+  
+  get '/signup' do
+    erb :'users/create_user'
+  end
+  
+  post '/signup' do
+    redirect "/tweets"
+  end
+  
+  get '/login' do
+    erb :'/users/login'
+  end
+  
+  post '/login' do
+     redirect "/tweets"
+  end
+  
+  get '/logout' do
+    redirect '/login'
+    redirect '/'
+  end
+  
+  get '/users/:slug' do
+    erb :'/users/show'
+  end
 
 end
